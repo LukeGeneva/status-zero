7a6b8838e2e40489d707bac31f30f6b992c18561
Luke Geneva <lukegeneva@gmail.com>
2020-02-27 06:32:44 -0600

    Switch config to YAML

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const moment = require('moment');
 const { exec } = require('child_process');
 const puppeteer = require('puppeteer');
-const dotenv = require('dotenv');
+const jsyaml = require('js-yaml');
+const path = require('path');
+const fs = require('fs');
 const { getPreviousWorkday } = require('./calendar');
 
-dotenv.config();
+const configPath = path.join(__dirname, 'config.yaml');
+const configDoc = fs.readFileSync(configPath);
+const config = jsyaml.safeLoad(configDoc);
 
 const previousWorkday = moment(getPreviousWorkday(new Date()));
 const after = previousWorkday.format('YYYY-MM-DD');
 const before = previousWorkday.format('YYYY-MM-DD 23:59:59');
-const command = `cd ~/dotfiles && git log --all --after='${after}' --before='${before}' --format='%s%n%b' --author='${process.env.GIT_AUTHOR}'`;
+const command = `cd ~/dotfiles && git log --all --after='${after}' --before='${before}' --format='%s%n%b' --author='${config.gitAuthor}'`;
 
 exec(command, {}, onGitLogComplete);
 
@@ -18,9 +22,9 @@ async function onGitLogComplete(error, stdout) {
   const page = await browser.newPage();
   await page.goto('https://statushero.com/signin');
   await page.focus('#user_email');
-  await page.keyboard.type(process.env.STATUSHERO_USERNAME);
+  await page.keyboard.type(config.statusHeroUsername);
   await page.focus('#user_password');
-  await page.keyboard.type(process.env.STATUSHERO_PASSWORD);
+  await page.keyboard.type(config.statusHeroPassword);
   await page.click('button[type="submit"]');
   await page.goto('https://statushero.com/teams/finance/statuses/current/edit');
   await page.focus('#answer_set_previous');
