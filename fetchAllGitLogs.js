f20c5e3a837a1c8c2b456b83cb8b1649de777171
Luke Geneva <lukegeneva@gmail.com>
2020-02-27 21:31:08 -0600

    Handle multiple git directories from config
    This change allows multiple directories to be read from the config file
    and retrieves the logs from each, combining them together with a
    blank line between.

--- fetchAllGitLogs.js
+++ fetchAllGitLogs.js
@@ -5,9 +5,16 @@ function fetchAllGitLogs({ day, author, directories }) {
   const workday = moment(day);
   const after = workday.format('YYYY-MM-DD');
   const before = workday.format('YYYY-MM-DD 23:59:59');
-  const command = `cd ~/dotfiles && git log --all --after='${after}' --before='${before}' --format='%s%n%b' --author='${author}'`;
-
-  return new Promise((resolve, reject) => exec(command, {}, (error, stdout) => (error ? reject(error) : resolve(stdout))));
+  // TODO: Use double quotes for args to handle names with apostrophes
+  const promises = directories.map(dir => {
+    const command = `cd ${dir} && git log --all --after='${after}' --before='${before}' --format='%s%n%b' --author='${author}'`;
+    return new Promise((resolve, reject) => exec(command, {}, (error, stdout) => (error ? reject(error) : resolve(stdout))));
+  });
+  return new Promise((resolve, reject) => {
+    Promise.all(promises)
+      .then(logs => resolve(logs.join('\n')))
+      .catch(reject);
+  });
 }
 
 module.exports = fetchAllGitLogs;
